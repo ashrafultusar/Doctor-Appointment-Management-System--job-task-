4346da3ebb9d0e6e158a893eee513bfd9a3530c8
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-09-03 15:23:18 +0600

    added all projects structure

---
+++ src/app/doctor/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            this is doctor dashnoard page
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/patient/appointments/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+          this is patients appointments page  
+        </div>
+    );
+};
+
+export default page;
